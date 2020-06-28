5cd01cecb784c70da82671e20a7742b472a96575
Leonard Olaf Paeleke <l.paeleke@campus.tu-berlin.de>
2020-06-28 21:27:53 +0200

    Update ps3_application.py for assignment 3

--- assignment3/stubs/ps3_application.py
+++ assignment3/stubs/ps3_application.py
@@ -22,11 +22,165 @@ import matplotlib as plt
 # from matplotlib.lines import Line2D
 from scipy.stats import norm
 import os
+import scipy.io as sio
 import pickle
 
 
 import ps3_implementation as imp
 imp = reload(imp)
+# ASSIGNMENT 3 CLASS 
+# contains all the the excercises of assignment 3
+class assignment3():
+    """
+    the object contains the tasks for assignment 3 of the ML Learning Course Excercise 3 
+    """
+    def __init__(self):
+        """
+        load data for assignment 3
+        CAVEAT path to data might be different 
+        """
+        # load data 
+        cwd = os.getcwd()
+        file_name = 'qm7.mat'
+        path_to_data = os.path.dirname(cwd) + '/data/'+file_name
+        assert os.path.exists(path_to_data), "The path does not excist."
+        data = sio.loadmat(path_to_data)
+        data_X = data['X'] # of shape 7165x23x23 
+        self.y = data['T'].T
+        # transform each msteolecule into a vector xi of R23 by eigenvalues of the M of R 23x23
+        self.X, eigenvec = np.linalg.eigh(data_X)
+
+        # Visualize data
+        #plt.scatter(data['X'], data['R'])
+    def distance_plot(self):
+        """ 
+        Excercise 3a
+        calculate pairwise distance of the dataset
+        """
+        # calculate pairwise distance
+        D = np.linalg.norm(self.X[None,:]-self.X[:,None], axis = 2)
+        # calculate absolute energy difference
+        y_abs_diff = abs(self.y[None,:,:] - self.y[:,:,None])
+        # plot
+        plt.figure(figsize=(8, 8))
+        plt.plot(D,y_abs_diff.reshape(len(self.y),len(self.y)),'bo');
+        plt.xlabel("||$x_{i}$ - $x_{j}$||")
+        plt.ylabel("|$y_{i}$ - $y_{j}$|")
+        
+    def split_data(self):
+        """
+        # Excercise 3b
+        Split data randomly into training set of 5000 and test of 2165 samples
+        seed = 2
+        """
+        # split data
+        # Random Partitioning
+        X_pos = np.linspace(0,len(self.X)-1, len(self.X))
+        random.Random(2).shuffle(X_pos)
+        self.Xtr = self.X[X_pos[:5000].astype('int')]
+        self.Xte = self.X[X_pos[5000:].astype('int')]
+        self.Ytr = self.y[X_pos[:5000].astype('int')]
+        self.Yte = self.y[X_pos[5000:].astype('int')]
+
+    def fold_cross_validation(self):
+        """
+        # Excercise 3c
+        Perform a cross validation on a training set of 2500
+        Perform split_data before
+        """
+        # Fivefold Cross validation
+        train_samples = random.sample(range(0, 5000), 2500)
+        Xtr2500 = self.Xtr[train_samples]
+        Ytr2500 = self.Ytr[train_samples]
+        D2500= np.linalg.norm(Xtr2500[None,:]-Xtr2500[:,None], axis = 2)
+        quantiles = np.quantile(D2500,[0.1,0.5, 0.9])
+        params = { 'kernel': ['gaussian'], 'kernelparameter': quantiles, 'regularization': np.logspace(-7,0, 10) }
+        self.cvkrr = imp.cv(Xtr2500, Ytr2500, imp.krr, params, loss_function=mean_absolute_error, nfolds=5)
+        y_pred2500 = self.cvkrr.predict(self.Xte)
+        MAE = mean_absolute_error(self.Yte, y_pred2500)
+        print("The mean absolute error is: {} ".format(round(MAE,2)))
+        print("The best regularzation parameter C is: {}".format(self.cvkrr.regularization))
+        print("The best kernelparameter sigma is: {}".format(self.cvkrr.kernelparameter))
+
+    def plot_MAE_for_different_nsamples(self):
+        """
+        # Excercise 3d
+        Plot MAE for different nsamples
+        Perform fold_cross_validation before
+        """
+        MAE = []
+        n_samples = [100,300,600,900,1200,1700,2000,2700,3000,3900,4200,4500,4700,4800,4900,4950,5000]
+        for i in tqdm(n_samples):
+            train_samples = random.sample(range(0, 5000), i)
+            Xtr_nsample = self.Xtr[train_samples]
+            Ytr_nsample = self.Ytr[train_samples]
+            model = imp.krr([self.cvkrr.kernel][0], [self.cvkrr.kernelparameter][0], [self.cvkrr.regularization])
+            model.fit(Xtr_nsample, Ytr_nsample)
+            y_pred = model.predict(self.Xte)
+            MAE.append(mean_absolute_error(self.Yte,y_pred))
+        plt.figure(figsize =(8,6))
+        plt.plot(n_samples, MAE, 'bo')
+        plt.xlabel("n training samples")
+        plt.ylabel("Mean Absolute Error [kcal/mol]")
+        
+    def plot_energies_for_1000(self):
+        """ 
+        Excercise 3e, perform under-, well- and overfit for 1000 training samples
+        """
+        # split data
+        # Random Partitioning
+        X_pos = np.linspace(0,len(self.X)-1, len(self.X))
+        random.Random(4).shuffle(X_pos)
+        Xtr1000 = self.X[X_pos[:1000].astype('int')]
+        Xte1000 = self.X[X_pos[1000:].astype('int')]
+        Ytr1000 = self.y[X_pos[:1000].astype('int')]
+        Yte1000 = self.y[X_pos[1000:].astype('int')]
+        
+        # get parameter for good fit
+        # Fivefold Cross validation
+
+        D1000= np.linalg.norm(Xtr1000[None,:]-Xtr1000[:,None], axis = 2)
+        quantiles = np.quantile(D1000,[0.1,0.5, 0.9])
+        params = { 'kernel': ['gaussian'], 'kernelparameter': quantiles, 'regularization': np.logspace(-7,0, 10) }
+        cvkrr = imp.cv(Xtr1000, Ytr1000, imp.krr, params, loss_function=mean_absolute_error, nfolds=5)
+        y_pred1000 = cvkrr.predict(Xte1000)
+        MAE = mean_absolute_error(Yte1000, y_pred1000)
+        
+        # result of CV
+        print("The mean absolute error is: {} ".format(round(MAE,2)))
+        print("The best regularzation parameter C is: {}".format(cvkrr.regularization))
+        print("The best kernelparameter sigma is: {}".format(cvkrr.kernelparameter))
+        print("The cvloss: {}".format(cvkrr.cvloss))
+        
+        # define parameters for training
+        params = { 'kernel': ['linear','gaussian', 'gaussian'], 'kernelparameter': [False, cvkrr.kernelparameter, 1], 'regularization': [cvkrr.regularization, cvkrr.regularization, 0] }
+        
+        # plot
+        plt.figure(figsize =(10,6))
+        for i in [0,1,2]:
+            model = imp.krr(params['kernel'][i], params['kernelparameter'][i], params['regularization'][i])
+            model.fit(Xtr1000,Ytr1000)
+            y_pred_train = model.predict(Xtr1000)
+            y_pred = model.predict(self.Xte)
+            plt.subplot(1,3,i+1)
+            plt.plot(self.Yte,y_pred,'bo')
+            plt.plot(Ytr1000,y_pred_train,'ro')
+            plt.xlabel("y_true")
+            plt.ylabel("y_pred")
+            plt.legend(labels = ['test', 'train'])
+        plt.tight_layout(pad=3.0)
+
+assign = assignment3()
+# Excecise 3a - takes long
+assign.distance_plot()
+# Excecise 3b
+assign.split_data()
+# Excecise 3c
+assign.fold_cross_validation()
+# Excecise 3d
+assign.plot_MAE_for_different_nsamples()
+# Excecise 3e
+assign.plot_energies_for_1000()
 
 # ASSIGNMENT 4 IMPLEMENTATION FUNCTIONS
 def roc_fun(y_true, y_pred, biases=100, threshold=0):
